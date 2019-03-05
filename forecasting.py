12f0bcfc77754afaff195882f822f15b63de07a3
Shih-Chen Lin <s1lin@eng.ucsd.edu>
2019-03-04 19:03:49 -0800

    add forecasting and forecasting image

--- forecasting.py
+++ forecasting.py
@@ -9,8 +9,15 @@ Created on Mon Mar  4 00:56:28 2019
 import numpy as np
 from pandas import Series
 import pandas as pd
-from statsmodels.tsa.arima_model import ARIMA
+#from statsmodels.tsa.arima_model import ARIMA
 from pyquery import PyQuery as pq
+#importing required libraries
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+import matplotlib
+matplotlib.use('TkAgg')  
+import matplotlib.pyplot as plt 
 
 
 # create a differenced series
@@ -73,7 +80,7 @@ def dict2df(dic):
 
 price_url = 'https://www.eia.gov/dnav/pet/hist/LeafHandler.ashx?n=PET&s=EMM_EPM0_PTE_R10_DPG&f=W'
 Region = ["East Coast", "Midwest", "Gulf Coast", "Rocky Mountain", "West Coast"]
-
+train_sample = 10
 if __name__ == "__main__":
     price_dict = {}
     
@@ -85,19 +92,95 @@ if __name__ == "__main__":
 
 #%%
 
+#creating train and test sets
+dataset = df_price[Region[0]].values
+new_data = df_price[Region[0]]
+dataset = dataset[:, None]
+train = dataset[0:300,:]
+valid = dataset[300:,:]
 
+#converting dataset into x_train and y_train
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(dataset)
 
-# load dataset
-series = Series.from_csv('dataset.csv', header=None)
-# seasonal difference
-X = series.values
-days_in_year = 365
-differenced = difference(X, days_in_year)
-# fit model
-model = ARIMA(differenced, order=(7,0,1))
-model_fit = model.fit(disp=0)
-# one-step out-of sample forecast
-forecast = model_fit.forecast()[0]
-# invert the differenced forecast to something usable
-forecast = inverse_difference(X, forecast, days_in_year)
-print('Forecast: %f' % forecast)
+x_train, y_train = [], []
+for i in range(train_sample,len(train)):
+    x_train.append(scaled_data[i-train_sample:i,0])
+    y_train.append(scaled_data[i,0])
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+x_train = np.reshape(x_train, (x_train.shape[0],x_train.shape[1],1))
+
+# create and fit the LSTM network
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1],1)))
+model.add(LSTM(units=50))
+model.add(Dense(1))
+
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(x_train, y_train, epochs=1, batch_size=1, verbose=2)
+
+#predicting 246 values, using past 60 from the train data
+inputs = new_data[len(df_price) - len(valid) - train_sample:len(df_price) - len(valid)].values
+#inputs = new_data[len(df_price) - len(valid) - 20:].values
+
+inputs = inputs.reshape(-1,1)
+inputs_list = []
+for i in range(train_sample):
+    inputs_list.append(float(inputs[i]))
+#inputs_list = inputs.tolist()
+tmp  = scaler.transform(inputs)
+inputs_list2 = []
+for i in range(train_sample):
+    inputs_list2.append(float(tmp[i]))
+#inputs_list2 = list(inputs_list2)
+
+#X_test = []
+#for i in range(20,inputs.shape[0]+1):
+#    X_test.append(inputs[i-20:i,0])
+#X_test = np.array(X_test)
+#
+#X_test = np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1))
+test_temp = []
+test = []
+for i in range(36):
+#    tmp = np.expand_dims(X_test[i, :, 0], axis=0)
+#    tmp = X_test[0, :, 0]
+    tmp = inputs_list2
+    temp_array = np.zeros(([1,train_sample, 1]))
+    temp_array[0, :, 0] = np.asarray(tmp).reshape([1, train_sample])
+    print(temp_array)
+#    temp_result = scaler.inverse_transform(model.predict(temp_array))
+    test_temp.append(scaler.inverse_transform(model.predict(temp_array)))
+#    print()
+    test.append(test_temp[-1][0][0])
+    inputs_list.pop(0)
+    inputs_list.append(float(test_temp[-1]))
+    print(i)
+    inputs_list0 = np.asarray(inputs_list).reshape(-1,1)
+    inputs_list2  = scaler.transform(inputs_list0)
+    inputs_list2 = list(inputs_list2)
+
+closing_price = test
+##closing_price = model.predict(X_test)
+#closing_price = 
+
+train = new_data[:300]
+#valid = new_data[300:]
+#valid = valid.to_frame()
+prediction = pd.DataFrame(closing_price, columns= ['prediction'] , index =pd.date_range(start='2018/4/1', periods=36, freq='MS'))
+#valid['Predictions'] = closing_price
+plt.figure()
+plt.plot(train)
+plt.plot(prediction)
+plt.title('Forecasting of the Gas Price')
+plt.xlabel('Year')
+plt.ylabel("Gas Price per gallon ($/gal)")
+#plt.savefig('./forecasting40.png')
+
+#
+#
+#
+#plt.figure()
+#plt.plot(range(closing_price.shape[0]), closing_price)
+#plt.show()
